3fa9413012cd0e012e014f7ee1b05a1fb9677146
Laszlo Kindrat <laszlokindrat@xmos.com>
2021-02-24 16:39:02 -0500

    refactored ElementwiseInit

--- tensorflow/lite/micro/kernels/xcore/xcore_add.cc
+++ tensorflow/lite/micro/kernels/xcore/xcore_add.cc
@@ -2,8 +2,8 @@
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/kernels/xcore/xcore_custom_options.h"
 #include "tensorflow/lite/micro/kernels/xcore/xcore_dispatcher.h"
+#include "tensorflow/lite/micro/kernels/xcore/xcore_elementwise.h"
 #include "tensorflow/lite/micro/kernels/xcore/xcore_ops.h"
 #include "tensorflow/lite/micro/kernels/xcore/xcore_utils.h"
 
@@ -64,27 +64,6 @@ struct AddOpData {
 // -------------------------------------------------------------------- //
 // op function implementations
 // -------------------------------------------------------------------- //
-
-void* Init(TfLiteContext* context, const char* buffer, size_t length) {
-  auto* op_data = construct_persistent_object<AddOpData>(context);
-
-  auto par_parser = CustomOptionParser(
-      CustomOptionParser(buffer, length).parseNamedCustomOption("par").AsMap());
-  auto job_sizes = par_parser.parseNamedCustomOption("eg").AsVector();
-  auto n_threads = par_parser.parseNamedCustomOption("th").AsInt32();
-  TFLITE_DCHECK_EQ(n_threads, job_sizes.size());
-
-  // in this op we have one job per thread
-  op_data->threads.allocate(context, n_threads);
-  int start_idx = 0;
-  for (int j{0}; j < n_threads; j++) {
-    auto job_size = job_sizes[j].AsInt32();
-    op_data->threads.append({start_idx, job_size, &op_data->args});
-    start_idx = start_idx + job_size;
-  }
-
-  return op_data;
-}
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   TF_LITE_ENSURE_EQ(context, NumInputs(node), 3);
@@ -147,7 +126,8 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
 }  // namespace add
 
 TfLiteRegistration* Register_Add_8() {
-  static TfLiteRegistration r = {add::Init, nullptr, add::Prepare, add::Eval};
+  static TfLiteRegistration r = {ElementwiseInit<add::AddOpData>, nullptr,
+                                 add::Prepare, add::Eval};
   return &r;
 }
 
